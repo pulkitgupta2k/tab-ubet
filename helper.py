8c8adc0f4fe4a63f0eed2bc21c55678bbdd20a9a
Pulkit Gupta <pulkitgupta2k@gmail.com>
2020-05-30 19:34:30 +0530

    handled none value json error

--- helper.py
+++ helper.py
@@ -21,7 +21,6 @@ def get_races():
     today = datetime.today().strftime("%Y/%m/%d")
     link = "https://tab.ubet.com/api/racing/racingdaysummary/{}/[]".format(
         today)
-    print(today)
     json_data = getJSON(link)
     json_data = json_data["RacingCodes"]
     racing_codes = []
@@ -32,23 +31,25 @@ def get_races():
             races = meeting["Races"]
             for race in races:
                 if race["Status"] == "SELLING":
+                    race_number =  race["RaceNumber"]
+                    if race_number < 10:
+                        race_number = "0" + str(race_number)
                     racing_codes.append(
-                        "{}/{}/{}:{}".format(today, meeting["Code"], race["RaceNumber"], code))
-    print(racing_codes)
+                        "{}/{}/{}:{}".format(today, meeting["Code"],race_number, code))
     return racing_codes
 
 
 def get_race_deatils(key):
     code = key.split(":")[1]
     key = key.split(":")[0]
-
+    
     link_race_inf = "https://tab.ubet.com/api/racingViewData/raceInformation/{}".format(
         key)
     link_tips = "https://tab.ubet.com/api/racingViewData/tips/{}".format(key)
     link_runner_inf = "https://tab.ubet.com/api/racingViewData/racing/{}".format(
         key)
-    link_runner_add = "https://api.beta.tab.com.au/v1/ubet-sky-vision-service/StreamFormGuide/{}{}/runners".format(
-        key.replace("/", ""), code)
+    link_runner_add = "https://api.beta.tab.com.au/v1/ubet-sky-vision-service/StreamFormGuide/{}{}/runners".format(key.replace("/", ""), code)
+    # print(link_runner_add)
 
     json_race_inf = getJSON(link_race_inf)
     json_tips = getJSON(link_tips)
@@ -78,35 +79,103 @@ def get_race_deatils(key):
     runner_adds = []
     for runner_add_json in json_runner_add:
         runner_add = {}
-        runner_add['tabNumber'] = runner_add_json['tabNumber']
-        runner_add['jockeyName'] = runner_add_json['jockeyName']
-        runner_add['prizeMoney'] = runner_add_json['prizeMoney']
-        runner_add['sire'] = runner_add_json['sire']
-        runner_add['dam'] = runner_add_json['dam']
-        runner_add['total'] = runner_add_json['total']
-        runner_add['winPercent'] = runner_add_json['winPercent']
-        runner_add['placePercent'] = runner_add_json['placePercent']
-        runner_add['resultAtThisDistance'] = runner_add_json['resultAtThisDistance']
-        # runner_add['thisTrack'] = runner_add_json['thisTrack']
-        runner_add['thisSeason'] = runner_add_json['thisSeason']
-        runner_add['track'] = runner_add_json['tabFormData']['track']
-        runner_add['distance'] = runner_add_json['tabFormData']['distance']
-        runner_add['trackAndDistance'] = runner_add_json['tabFormData']['trackAndDistance']
-        runner_add['class'] = runner_add_json['tabFormData']['class']
-        runner_add['firstUp'] = runner_add_json['tabFormData']['firstUp']
-        runner_add['secondUp'] = runner_add_json['tabFormData']['secondUp']
-        runner_add['jockey'] = runner_add_json['tabFormData']['jockey']
-        runner_add['firm'] = runner_add_json['tabFormData']['firm']
-        runner_add['good'] = runner_add_json['tabFormData']['good']
-        runner_add['soft'] = runner_add_json['tabFormData']['soft']
-        runner_add['heavy'] = runner_add_json['tabFormData']['heavy']
+        # pprint(runner_add_json.keys())
+        try:
+            runner_add['tabNumber'] = runner_add_json['tabNumber']
+        except:
+            runner_add['tabNumber'] = ""
+        try:
+            runner_add['jockeyName'] = runner_add_json['jockeyName']
+        except:
+            runner_add['jockeyName'] = ""
+        try:
+            runner_add['prizeMoney'] = runner_add_json['prizeMoney']
+        except:
+            runner_add['prizeMoney'] = ""
+        try:
+            runner_add['sire'] = runner_add_json['sire']
+        except:
+            runner_add['sire'] = ""
+        try:
+            runner_add['dam'] = runner_add_json['dam']
+        except:
+            runner_add['dam'] = ""
+        try:
+            runner_add['total'] = runner_add_json['total']
+        except:
+            runner_add['total'] = ""
+        try:
+            runner_add['winPercent'] = runner_add_json['winPercent']
+        except:
+            runner_add['winPercent'] = ""
+        try:
+            runner_add['placePercent'] = runner_add_json['placePercent']
+        except:
+            runner_add['placePercent'] = ""
+        try:
+            runner_add['resultAtThisDistance'] = runner_add_json['resultAtThisDistance']
+        except:
+            runner_add['resultAtThisDistance'] = ""
+        try:
+            runner_add['thisTrack'] = runner_add_json['thisTrack']
+        except:
+            runner_add['thisTrack'] = ""
+        try:
+            runner_add['thisSeason'] = runner_add_json['thisSeason']
+        except:
+            runner_add['thisSeason'] = ""
+        try:
+            runner_add['track'] = runner_add_json['tabFormData']['track']
+        except:
+            runner_add['track'] = ""
+        try:
+            runner_add['distance'] = runner_add_json['tabFormData']['distance']
+        except:
+            runner_add['distance'] = ""
+        try:
+            runner_add['trackAndDistance'] = runner_add_json['tabFormData']['trackAndDistance']
+        except:
+            runner_add['trackAndDistance'] = ""
+        try:
+            runner_add['class'] = runner_add_json['tabFormData']['class']
+        except:
+            runner_add['class'] = ""
+        try:
+            runner_add['firstUp'] = runner_add_json['tabFormData']['firstUp']
+        except:
+            runner_add['firstUp'] = ""
+        try:
+            runner_add['secondUp'] = runner_add_json['tabFormData']['secondUp']
+        except:
+            runner_add['secondUp'] = ""
+        try:
+            runner_add['jockey'] = runner_add_json['tabFormData']['jockey']
+        except:
+            runner_add['jockey'] = ""
+        try:
+            runner_add['firm'] = runner_add_json['tabFormData']['firm']
+        except:
+            runner_add['firm'] = ""
+        try:
+            runner_add['good'] = runner_add_json['tabFormData']['good']
+        except:
+            runner_add['good'] = ""
+        try:
+            runner_add['soft'] = runner_add_json['tabFormData']['soft']
+        except:
+            runner_add['soft'] = ""
+        try:
+            runner_add['heavy'] = runner_add_json['tabFormData']['heavy']
+        except:
+            runner_add['heavy'] = ""
+
         runner_adds.append(runner_add)
 
     for index, runner in enumerate(runners):
         runners[index].update(runner_adds[index])
 
     race_details["runners"] = runners
-    pprint(race_details)
+    # pprint(race_details)
     return race_details
 
 
@@ -114,7 +183,7 @@ def make_matrix(details):
     matrix = []
     heading = ["Race Name", "Race Distance", "Tips", "Tipster", "Runner Name", "Rider Name", "Trainer Name",
                "Rating", "Last Three Starts", "Form", "Weight", "Prize Money", "Sire", "Dam", "Total", "Win Percent", 
-               "Place Percent", "Result", "This Season", "Track", "Distance", "Track and Distance", "Class", "First Up", "Second Up",
+               "Place Percent", "Result","This Track", "This Season", "Track", "Distance", "Track and Distance", "Class", "First Up", "Second Up",
                "Jockey", "Firm", "Good", "Soft", "Heavy"]
     matrix.append(heading)
 
@@ -141,6 +210,7 @@ def make_matrix(details):
 
             row.append(runner["placePercent"])
             row.append(runner["resultAtThisDistance"])
+            row.append(runner["thisTrack"])
             row.append(runner["thisSeason"])
             row.append(runner["track"])
             row.append(runner["distance"])
@@ -162,6 +232,7 @@ def driver():
     details = []
     racing_codes = get_races()
     for racing_code in racing_codes:
+        print(racing_code)
         detail = get_race_deatils(racing_code)
         details.append(detail)
     matrix = make_matrix(details)
