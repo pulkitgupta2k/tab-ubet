cbd7bcf610217cd74772b3a816004c7644aeb6a7
Pulkit Gupta <pulkitgupta2k@gmail.com>
2020-05-31 13:05:00 +0530

    added columns

--- helper.py
+++ helper.py
@@ -60,30 +60,55 @@ def get_race_deatils(key):
     json_runner_add = getJSON(link_runner_add)
 
     race_details = {}
+    race_details["raceCode"] = key.split('/')[-1]
+    race_details["raceLocation"] = json_race_inf["meetingName"]
     race_details["raceNumber"] = json_race_inf["raceNumber"]
     race_details["raceDistance"] = json_race_inf["raceDistance"]
     race_details["raceName"] = json_race_inf["raceName"]
     race_details["raceTime"] = json_race_inf["raceDate"].split("T")[1][:-1]
     try:
-        race_details["tips"] = json_tips["racingTips"][0]["tipsData"][0]["tips"]
+        race_details["tips_1"] = json_tips["racingTips"][0]["tipsData"][0]["tips"]
     except:
-        race_details["tips"] = ""
+        race_details["tips_1"] = ""
     try:
-        race_details["tipster"] = json_tips["racingTips"][0]["tipsData"][0]["tipster"]
+        race_details["tipster_1"] = json_tips["racingTips"][0]["tipsData"][0]["tipster"]
     except:
-        race_details["tipster"] = ""
+        race_details["tipster_1"] = ""
+
+    try:
+        race_details["tips_2"] = json_tips["racingTips"][0]["tipsData"][0]["tips"]
+    except:
+        race_details["tips_2"] = ""
+    try:
+        race_details["tipster_2"] = json_tips["racingTips"][0]["tipsData"][0]["tipster"]
+    except:
+        race_details["tipster_2"] = ""
+
+    try:
+        race_details["tips_3"] = json_tips["racingTips"][0]["tipsData"][0]["tips"]
+    except:
+        race_details["tips_3"] = ""
+    try:
+        race_details["tipster_3"] = json_tips["racingTips"][0]["tipsData"][0]["tipster"]
+    except:
+        race_details["tipster_3"] = ""
 
     runners = []
     for runner_json in json_runner_inf["runners"]:
         runner = {}
         runner['runnerNumber'] = runner_json['runnerInformation']['runnerNumber']
         runner['runnerName'] = runner_json['runnerInformation']['runnerName']
+        runner['barrier'] = runner_json['runnerInformation']['barrier']
         runner['riderName'] = runner_json['runnerInformation']['riderName']
         runner['trainerName'] = runner_json['runnerInformation']['trainerName']
         runner['rating'] = runner_json['rating']
         runner['lastThreeStarts'] = runner_json["lastThreeStarts"]
         runner['form'] = runner_json['form']
         runner['weight'] = runner_json['runnerInformation']['weight']
+        runner['toteWin'] = runner_json['tote']['toteWin']
+        runner['totePlace'] = runner_json['tote']['totePlace']
+        runner['fixedWin'] = runner_json['fixedPrice']['fixedWin']
+        runner['fixedPlace'] = runner_json['fixedPrice']['fixedPlace']
         runners.append(runner)
 
     runner_adds = []
@@ -135,6 +160,12 @@ def get_race_deatils(key):
         except:
             runner_add['thisSeason'] = ""
         try:
+            runner_add['last20Starts'] = runner_add_json['tabFormData']['last20StartsFirstPart'] + \
+                ' ' + runner_add_json['tabFormData']['last20StartsSecondPart']
+        except:
+            runner_add['last20Starts'] = ""
+
+        try:
             runner_add['track'] = runner_add_json['tabFormData']['track']
         except:
             runner_add['track'] = ""
@@ -179,6 +210,27 @@ def get_race_deatils(key):
         except:
             runner_add['heavy'] = ""
 
+        try:
+            runner_add['age'] = runner_add_json['age']
+        except:
+            runner_add['age'] = ""
+
+        try:
+            gender = runner_add_json['gender']
+            if gender == 'M':
+                gender = 'Mare'
+            if gender == 'G':
+                gender = 'Gelding'
+            if gender == 'B':
+                gender = 'Bitch'
+            if gender == 'D':
+                gender == 'Dog'
+            if gender == 'H':
+                gender == 'Horse'
+            runner_add['gender'] = runner_add_json['gender']
+        except:
+            runner_add['gender'] = ""
+
         runner_adds.append(runner_add)
 
     for index, runner in enumerate(runners):
@@ -191,46 +243,77 @@ def get_race_deatils(key):
 
 def make_matrix(details):
     matrix = []
-    heading = ["Race Number", "Race Name", "Race Time", "Race Distance", "Tip 1", "Tip 2", "Tip 3", "Tip 4", "Tipster", "Runner Name", "Rider Name", "Trainer Name",
-               "Rating", "Last Three Starts", "Form", "Weight", "Prize Money", "Sire", "Dam", "Total", "Win Percent",
-               "Place Percent", "Result_1", "Result_2", "Result_3", "This Track_1", "This Track_2", "This Track_3", 
-               "This Season_1", "This Season_2", "This Season_3", "This Season_4", "Track_1", "Track_2", "Track_3", 
-               "Distance_1", "Distance_2", "Distance_3", "Track and Distance_1", "Track and Distance_2", "Track and Distance_3", 
-               "Class_1","Class_2","Class_3", "First Up_1", "First Up_2", "First Up_3", "Second Up_1", "Second Up_2", "Second Up_3",
-               "Jockey_1","Jockey_2","Jockey_3", "Firm_1", "Firm_2", "Firm_3", "Good_1", "Good_2", "Good_3", "Soft_1", "Soft_2", "Soft_3", "Heavy_1", "Heavy_2", "Heavy_3"]
+    heading = ["Race Code", "Race Venue", "Race Number", "Race Name", "Race Time", "Race Distance",
+               "Tipster_1", "Tip 1", "Tip 2", "Tip 3", "Tip 4",
+               "Tipster_2", "Tip 5", "Tip 6", "Tip 7", "Tip 8",
+               "Tipster_3", "Tip 9", "Tip 10", "Tip 11", "Tip 12",
+               "Runner Number", "Runner Name", "Runner Barrier", "Rider Name", "Trainer Name",
+               "Rating", "Last Three Starts", "Last 20 Starts", "Form", "Weight", "Age", "Gender", "Prize Money", "Sire", "Dam", "Total_1", "Total_2", "Total_3", "Win Percent",
+               "Place Percent", "Result_1", "Result_2", "Result_3", "This Track_1", "This Track_2", "This Track_3",
+               "This Season_1", "This Season_2", "This Season_3", "This Season_4", "Track_1", "Track_2", "Track_3",
+               "Distance_1", "Distance_2", "Distance_3", "Track and Distance_1", "Track and Distance_2", "Track and Distance_3",
+               "Class_1", "Class_2", "Class_3", "First Up_1", "First Up_2", "First Up_3", "Second Up_1", "Second Up_2", "Second Up_3",
+               "Jockey_1", "Jockey_2", "Jockey_3", "Firm_1", "Firm_2", "Firm_3", "Good_1", "Good_2", "Good_3", "Soft_1", "Soft_2", "Soft_3", "Heavy_1", "Heavy_2", "Heavy_3",
+               "Tote Win", "Tote Place", "Fixed Win", "Fixed Place"]
     matrix.append(heading)
 
     for detail in details:
         for runner in detail["runners"]:
             row = []
+            row.append(detail["raceCode"])
+            row.append(detail["raceLocation"])
             row.append(detail["raceNumber"])
             row.append(detail["raceName"])
             row.append(detail["raceTime"])
             row.append(detail["raceDistance"])
 
-            tips = detail["tips"].split("-")
+            row.append(detail["tipster_1"])
+            tips = detail["tips_1"].split("-")
             while not len(tips) >= 4:
                 tips.append("")
             row.extend(tips)
 
-            row.append(detail["tipster"])
+            row.append(detail["tipster_2"])
+            tips = detail["tips_2"].split("-")
+            while not len(tips) >= 4:
+                tips.append("")
+            row.extend(tips)
+
+            row.append(detail["tipster_3"])
+            tips = detail["tips_3"].split("-")
+            while not len(tips) >= 4:
+                tips.append("")
+            row.extend(tips)
+
+            row.append(runner["runnerNumber"])
             row.append(runner["runnerName"])
+            row.append(runner["barrier"])
+
             row.append(runner["riderName"])
             row.append(runner["trainerName"])
 
             row.append(runner["rating"])
             row.append(runner["lastThreeStarts"])
+            row.append(runner["last20Starts"])
             row.append(runner["form"])
             row.append(runner["weight"])
+            row.append(runner["age"])
+            row.append(runner["gender"])
             row.append(runner["prizeMoney"])
             row.append(runner["sire"])
             row.append(runner["dam"])
-            row.append(runner["total"])
-            row.append(runner["winPercent"])
 
+            results = runner["total"].replace(
+                "-", ":").split(":")
+            while not len(results) >= 3:
+                results.append("")
+            row.extend(results)
+
+            row.append(runner["winPercent"])
             row.append(runner["placePercent"])
 
-            results = runner["resultAtThisDistance"].replace("-", ":").split(":")
+            results = runner["resultAtThisDistance"].replace(
+                "-", ":").split(":")
             while not len(results) >= 3:
                 results.append("")
             row.extend(results)
@@ -299,6 +382,11 @@ def make_matrix(details):
             while not len(results) >= 3:
                 results.append("")
             row.extend(results)
+
+            row.append(runner["toteWin"])
+            row.append(runner["totePlace"])
+            row.append(runner["fixedWin"])
+            row.append(runner["fixedPlace"])
 
             matrix.append(row)
     return matrix
